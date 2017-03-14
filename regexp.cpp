1f82cf1ce78c99a1748186da8bd2b315b16c54d7
Lukas Zeller <luz@plan44.ch>
2017-03-14 23:48:37 +0100

    regexp/slre: added older free version of SLRE regex and simple wrapper class for it

---
+++ regexp.cpp
@@ -0,0 +1,81 @@
+//
+//  Copyright (c) 2017 plan44.ch / Lukas Zeller, Zurich, Switzerland
+//
+//  Author: Lukas Zeller <luz@plan44.ch>
+//
+//  This file is part of p44utils.
+//
+//  p44utils is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  p44utils is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with p44utils. If not, see <http://www.gnu.org/licenses/>.
+//
+
+#include "regexp.hpp"
+
+using namespace p44;
+
+
+RegExp::RegExp() :
+  capturesP(NULL)
+{
+  memset(&slre_state, 0, sizeof(slre_state));
+}
+
+
+RegExp::~RegExp()
+{
+  if (capturesP) delete capturesP; capturesP = NULL;
+}
+
+
+ErrorPtr RegExp::compile(const string aRegExp)
+{
+  ErrorPtr err;
+  if (!slre_compile(&slre_state, aRegExp.c_str())) {
+    err = Error::err<RegExpError>(RegExpError::Syntax, "RegExp Syntaxt error: %s", slre_state.err_str);
+    slre_state.num_caps = 0;
+  }
+  return err;
+}
+
+
+bool RegExp::match(const string aText, bool aCapture)
+{
+  typedef struct cap cap;
+  cap *capsP = NULL;
+  if (aCapture) {
+    // we need captures
+    capsP = new cap[slre_state.num_caps+1];
+  }
+  bool matched = slre_match(&slre_state, aText.c_str(), (int)aText.size(), capsP);
+  if (matched && capsP) {
+    if (!capturesP) {
+      capturesP = new StringVector;
+    }
+    else {
+      capturesP->clear();
+    }
+    for (int i=0; i<=slre_state.num_caps; i++) {
+      capturesP->push_back(string(capsP[i].ptr, capsP[i].len));
+    }
+  }
+  delete [] capsP;
+  return matched;
+}
+
+
+string RegExp::getCapture(int aIndex) const
+{
+  if (!capturesP || aIndex>=capturesP->size())
+    return "";
+  return (*capturesP)[aIndex];
+}
---
+++ regexp.hpp
@@ -0,0 +1,88 @@
+//
+//  Copyright (c) 2017 plan44.ch / Lukas Zeller, Zurich, Switzerland
+//
+//  Author: Lukas Zeller <luz@plan44.ch>
+//
+//  This file is part of p44utils.
+//
+//  p44utils is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  p44utils is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with p44utils. If not, see <http://www.gnu.org/licenses/>.
+//
+
+#ifndef __p44utils__regexp__
+#define __p44utils__regexp__
+
+#include "p44utils_common.hpp"
+#include <string>
+
+extern "C" {
+  #include "slre.h"
+}
+
+using namespace std;
+
+namespace p44 {
+
+
+  class RegExpError : public Error
+  {
+  public:
+    // Errors
+    typedef enum {
+      OK,
+      Syntax
+    } ErrorCodes;
+
+    static const char *domain() { return "RegExp"; }
+    virtual const char *getErrorDomain() const { return RegExpError::domain(); };
+    RegExpError(ErrorCodes aError) : Error(ErrorCode(aError)) {};
+  };
+
+
+
+  class RegExp : public P44Obj
+  {
+    struct slre slre_state;
+    typedef vector<string> StringVector;
+
+    StringVector *capturesP;
+
+  public:
+
+    RegExp();
+    virtual ~RegExp();
+
+    /// compile a regular expression (prepare for applying it with match())
+    /// @param aRegExp to compile
+    ErrorPtr compile(const string aRegExp);
+
+    /// Match regular expression
+    /// @param aCapture if true, results will be captured
+    /// @return true if match, false if not
+    bool match(const string aText, bool aCapture);
+
+    /// get captured substring from last match()
+    /// @param aIndex 0 for complete match string, 1..n for subcaptures
+    /// @return capture string or empty string if aIndex is out of range
+    string getCapture(int aIndex) const;
+
+  };
+  typedef boost::intrusive_ptr<RegExp> RegExpPtr;
+
+  
+
+} // namespace p44
+
+
+
+#endif /* defined(__p44utils__regexp__) */
