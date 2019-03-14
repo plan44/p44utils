6232c77ad6ad2f5e79e6c2df647291664736f784
Lukas Zeller <luz@plan44.ch>
2019-03-14 17:18:00 +0100

    ESP32: JSON support with jsonobject/json-c

--- component.mk
+++ component.mk
@@ -2,8 +2,33 @@
 # Component Makefile for using p44utils on ESP32 IDF platform
 #
 
-#COMPONENT_SRCDIRS := . slre sqlite3pp
-COMPONENT_SRCDIRS := . thirdparty/esp32_ws281x
+LEDCHAIN_TPDIR = thirdparty/esp32_ws281x
+LEDCHAIN_OBJS = \
+  ledchaincomm.o \
+  $(LEDCHAIN_TPDIR)/esp32_ws281x.o
+
+JSON_TPDIR = thirdparty/json-c
+JSON_OBJS = \
+  jsonobject.o \
+  jsoncomm.o \
+  jsonrpccomm.o \
+  $(JSON_TPDIR)/arraylist.o \
+  $(JSON_TPDIR)/debug.o \
+  $(JSON_TPDIR)/json_c_version.o \
+  $(JSON_TPDIR)/json_object.o \
+  $(JSON_TPDIR)/json_object_iterator.o \
+  $(JSON_TPDIR)/json_tokener.o \
+  $(JSON_TPDIR)/json_util.o \
+  $(JSON_TPDIR)/linkhash.o \
+  $(JSON_TPDIR)/printbuf.o \
+  $(JSON_TPDIR)/random_seed.o
+
+
+COMPONENT_SRCDIRS := \
+  . \
+  $(LEDCHAIN_TPDIR)/esp32_ws281x \
+  $(JSON_TPDIR)
+
 
 COMPONENT_OBJS:= \
   application.o \
@@ -14,14 +39,19 @@ COMPONENT_OBJS:= \
   mainloop.o \
   fdcomm.o \
   socketcomm.o \
-  $(call compile_only_if,$(CONFIG_P44UTILS_ENABLE_LEDCHAIN),ledchaincomm.o thirdparty/esp32_ws281x/esp32_ws281x.o)
+  $(call compile_only_if,$(CONFIG_P44UTILS_ENABLE_LEDCHAIN),$(LEDCHAIN_OBJS)) \
+  $(call compile_only_if,$(CONFIG_P44UTILS_ENABLE_JSON),$(JSON_OBJS))
 
-# %%%ugly hack for now%%% just take a boost checkout we already have
+
 COMPONENT_ADD_INCLUDEDIRS = \
   . \
-  thirdparty/esp32_ws281x
+  thirdparty \
+  $(LEDCHAIN_TPDIR) \
+  $(JSON_TPDIR)
 
 CPPFLAGS += \
   -Wno-reorder \
   -isystem \
   /Volumes/CaseSens/openwrt/build_dir/target-mipsel_24kc_musl/boost_1_67_0
+
+# %%%ugly hack above ^^^^^^ for now%%% just take a boost checkout we already have
