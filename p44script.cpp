plan44/p44utils

commit 2d82f429e18fb8ba3263f712236d6ffdfc5d4d57

p44script: capture "on" triggers only when actually being executed
Before, conditional handler definition (enclosed in an "if" or "try" block) did not work as expected

p44script.cpp
@@ -4144,7 +4144,7 @@ void SourceProcessor::defineTrigger(bool aGlobal)
     return;
   }
   CompiledTriggerPtr trigger;
-  if (!compiling()) // all handlers are context local and captured not before actually executed
+  if (!compiling() && !mSkipping) // all handlers are context local and captured not before actually executed
   {
     trigger = new CompiledTrigger("trigger", getTriggerAndHandlerMainContext());
     mResult = captureCode(trigger);

commit 0d94dbf2c08f1b12aff1abe840da83db4ca94577

p44script: fix global handlers
- was broken at least since: 2d82f42 - p44script: capture "on" triggers only when actually being executed
- cleanup of not-yet-finished elimination of declaration vs body part

p44script.cpp
@@ -4375,8 +4375,10 @@ void SourceProcessor::defineTrigger(bool aGlobal)
     return;
   }
   CompiledTriggerPtr trigger;
-  if (!compiling() && !mSkipping) // all handlers are context local and captured not before actually executed
-  {
+  if (
+    (compiling() && aGlobal) || // global handlers are captured at compilation
+    (!compiling() && !mSkipping && !aGlobal) // context local handlers must not be captured not before actually executed
+  ) {
     trigger = new CompiledTrigger("trigger", getTriggerAndHandlerMainContext());
     mResult = captureCode(trigger);
   }
@@ -4475,11 +4477,9 @@ void SourceProcessor::defineHandler(bool aGlobal)
     handler->installAndInitializeTrigger(mOlderResult);
     storeHandler();
   }
-  else {
-    checkAndResume();
-  }
   // back to where we were before
   pop();
+  checkAndResume();
 }
 
 // MARK: Statements
@@ -6037,15 +6037,8 @@ void ScriptCompiler::storeFunction()
 void ScriptCompiler::storeHandler()
 {
   if (!mResult->isErr()) {
-    // only handlers in declaration part must be stored at compile time
-    if (mEvaluationFlags & sourcecode) {
-      mResult = mDomain->registerHandler(mResult);
-    }
-    else {
-      // handler in script body must NOT be stored but discarded
-      mResult->deactivate();
-      mResult.reset();
-    }
+    // we only get here for global handlers, context handlers are stored when executed
+    mResult = mDomain->registerHandler(mResult);
   }
   checkAndResume();
 }

p44script.hpp
@@ -104,7 +104,6 @@ namespace p44 { namespace P44Script {
   typedef boost::intrusive_ptr<ScriptHost> ScriptHostPtr;
 
   class SourceProcessor;
-  typedef boost::intrusive_ptr<SourceProcessor> SourceProcessorPtr;
   class Compiler;
   class ScriptCodeThread;
   typedef boost::intrusive_ptr<ScriptCodeThread> ScriptCodeThreadPtr;